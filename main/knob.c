a92dfb4fe8eabcabf0ee436cb36ccdc258a7c9be
Huma Feng <hfe@bsab.local>
2026-01-05 10:05:28 +0800

    1.register the knob left and right callback functions.

--- main/knob.c
+++ main/knob.c
@@ -7,6 +7,17 @@
 
 #define TAG "main_knob"
 
+static void knob_left_cb(void *arg, void *data)
+{
+    ESP_LOGI(TAG, "KNOB: KNOB_LEFT,count_value:%d", iot_knob_get_count_value((knob_handle_t)arg));
+}
+
+static void knob_right_cb(void *arg, void *data)
+{
+    ESP_LOGI(TAG, "KNOB: KNOB_RIGHT,count_value:%d", iot_knob_get_count_value((knob_handle_t)arg));
+}
+
+
 esp_err_t knob_init(void)
 {
     // create knob
@@ -21,6 +32,22 @@ esp_err_t knob_init(void)
         return ESP_FAIL;
     } else {
         ESP_LOGI(TAG, "knob create success");
+    }
+
+    esp_err_t ret = ESP_FAIL;
+    ret = iot_knob_register_cb(s_knob, KNOB_LEFT, knob_left_cb, NULL);
+    if(ret != ESP_OK) {
+        ESP_LOGE(TAG, "knob register left callback function failed");
+        return ESP_FAIL;
+    } else {
+        ESP_LOGE(TAG, "knob register left callback function success");
+    }
+    ret = iot_knob_register_cb(s_knob, KNOB_RIGHT, knob_right_cb, NULL);
+    if(ret != ESP_OK) {
+        ESP_LOGE(TAG, "knob register right callback function failed");
+        return ESP_FAIL;
+    } else {
+        ESP_LOGE(TAG, "knob register right callback function success");
     }
 
     return ESP_OK;
